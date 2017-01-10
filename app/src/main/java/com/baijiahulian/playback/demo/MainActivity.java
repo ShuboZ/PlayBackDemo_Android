5654b57fad33aad38ca39d87bd2eb57f6ea099e2
yanglei <yanglei@baijiahulian.com>
2017-01-10 14:14:24 +0800

    readme

--- app/src/main/java/com/baijiahulian/playback/demo/MainActivity.java
+++ app/src/main/java/com/baijiahulian/playback/demo/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements LPLaunchListener 
         });
 
         long classId = getIntent().getLongExtra("classId", 0);
-        //1. 创建房间。 PS： sign 参数暂时无用，可以传任意字符串
+        //1. 创建房间。
         mRoom = LivePlaybackSDK.newPlayBackRoom(this, 32958737L, classId, LPConstants.LPDeployType.Test);
 
         //2. 进入房间
